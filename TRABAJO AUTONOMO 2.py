2340222014b9427da92482c7ef3cf9aa265cdeb8
NicolePamelaAndrade1012 <151667954+NicolePamelaAndrade1012@users.noreply.github.com>
2023-11-24 23:33:46 -0500

    Add files via upload

---
+++ TRABAJO AUTONOMO 2.py
@@ -0,0 +1,12 @@
+print("Hola Bienvenido a NIRST generador de contraseñas seguras")
+a="Ingresar usuario"
+b="(Digite número o email)"
+c="Ingresar contraseña"
+d="(La contraseña distingue de Mayúsculas o Minúsculas)"
+print(a,b)
+a1="En caso de no estar registrado puede hacerlo con su"
+b1="Email"
+c1="Número de celular"
+print(a1+" "+b1+"o"+c1)
+print("Nombres completos")
+print("Número de telefono o email")
